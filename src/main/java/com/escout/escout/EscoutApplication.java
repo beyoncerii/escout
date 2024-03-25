0213d95667c0cc2e5947e98ea54e7cf9fc743fb6
beyoncerii <2021614578@student.uitm.edu.my>
2024-03-25 17:37:42 +0800

    first commit

---
+++ src/main/java/com/escout/escout/EscoutApplication.java
@@ -0,0 +1,13 @@
+package com.escout.escout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escout/escout/EscoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escout.escout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
